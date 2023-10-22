a4d31ba9ae0152a43bd99567164ab7daf2ee5144
fadilgm24 <146375504+fadilgm24@users.noreply.github.com>
2023-10-23 05:30:32 +0700

    Update roles.h

--- src/player/objects/roles.h
+++ src/player/objects/roles.h
@@ -5,21 +5,21 @@
 namespace GTServer {
     enum ePlayerRole {
         PLAYER_ROLE_DEFAULT,
-        PLAYER_ROLE_VIP,
-        PLAYER_ROLE_VIP_PLUS,
         PLAYER_ROLE_MODERATOR,
-        PLAYER_ROLE_ADMINISTRATOR,
-        PLAYER_ROLE_MANAGER,
-        PLAYER_ROLE_DEVELOPER
+        PLAYER_ROLE_SUPER_MODERATOR,
+        PLAYER_ROLE_OWNER,
+        PLAYER_ROLE_SUPER_OWNER,
+        PLAYER_ROLE_ADMIN,
+        PLAYER_ROLE_DEVELOPER, FADILGM
     };
 
     static std::unordered_map<int, char> role_color = {
         { PLAYER_ROLE_DEFAULT, 'w' },
-        { PLAYER_ROLE_VIP, '1' }, 
-        { PLAYER_ROLE_VIP_PLUS, '4' }, 
-        { PLAYER_ROLE_MODERATOR, '#' },
-        { PLAYER_ROLE_ADMINISTRATOR, '9' },
-        { PLAYER_ROLE_MANAGER, '6' },
+        { PLAYER_ROLE_MODERATOR, '1' }, 
+        { PLAYER_ROLE_SUPER_MODERATOR, '4' }, 
+        { PLAYER_ROLE_OWNER, '#' },
+        { PLAYER_ROLE_SUPER_OWNER, '9' },
+        { PLAYER_ROLE_ADMIN, '6' },
         { PLAYER_ROLE_DEVELOPER, '6' }
     };
 }
